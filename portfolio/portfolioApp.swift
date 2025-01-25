369a73d4cb04527ae1919fb167cbe79d18c5985e
henriquepablo <pablo269704@gmail.com>
2025-01-25 12:19:25 -0300

    Initial Commit

---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by pablo henrique on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ portfolioTests/portfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioTests.swift
+//  portfolioTests
+//
+//  Created by pablo henrique on 25/01/25.
+//
+
+import Testing
+@testable import portfolio
+
+struct portfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
